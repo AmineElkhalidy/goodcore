cfc316b1c3f24bae40240980bed2fd5026ecfcd6
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2023-02-04 09:38:20 +0100

    Pushing the latest changes of navbar

--- src/app/header.js
+++ src/app/header.js
@@ -15,6 +15,8 @@ import {
   ChevronDoubleDownIcon,
   ChevronDownIcon,
   XMarkIcon,
+  EnvelopeIcon,
+  DevicePhoneMobileIcon,
 } from "@heroicons/react/24/outline";
 
 const Header = () => {
@@ -23,6 +25,32 @@ const Header = () => {
 
   return (
     <header className="absolute top-0 left-0 z-50 w-full bg-white">
+      {/* Top links */}
+      <div className="hidden w-full md:block bg-[#0b5492] py-2">
+        <div className="mr-10">
+          <div className="flex justify-end gap-6 text-white">
+            <Link
+              className="inline-flex items-center gap-1 text-xs uppercase group"
+              href={"mailto:contact@goodcore.co.uk"}
+            >
+              <EnvelopeIcon className="w-4 h-4" />{" "}
+              <span className="group-hover:underline">
+                contact@goodcore.co.uk
+              </span>
+            </Link>
+
+            <Link
+              className="inline-flex items-center gap-1 text-xs uppercase group"
+              href={"tel:02087816903"}
+            >
+              <DevicePhoneMobileIcon className="w-4 h-4" />{" "}
+              <span className="group-hover:underline">020 8781 6903</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* Navigation menu */}
       <nav className="container flex items-center justify-between h-16 px-4 mx-auto lg:max-w-6xl">
         <Link className="cursor-pointer" href={"#"}>
           <Image className="w-[70%]" src={Logo} alt="Header logo" />
@@ -57,6 +85,45 @@ const Header = () => {
               >
                 services <ChevronDownIcon className="w-4 h-4" />
               </Link>
+
+              <div className="absolute -left-[25rem] top-[-200%] group-hover:top-16">
+                <div className="z-50">
+                  {/* Grid container for 3 divs */}
+                  <div className="grid">
+                    <div className="flex flex-col items-center justify-between px-6 bg-orange-500 ">
+                      {/* Images container */}
+                      <div className="flex">
+                        <div className="overflow-hidden border-white rounded-full w-14 h-14">
+                          <Image
+                            className="w-full h-full bg-blue-300"
+                            src={""}
+                          />
+                        </div>
+                        <div className="overflow-hidden border-white rounded-full w-14 h-14">
+                          <Image
+                            className="w-full h-full bg-blue-300"
+                            src={""}
+                          />
+                        </div>
+                        <div className="overflow-hidden border-white rounded-full w-14 h-14">
+                          <Image
+                            className="w-full h-full bg-blue-300"
+                            src={""}
+                          />
+                        </div>
+                        <div className="overflow-hidden border-white rounded-full w-14 h-14">
+                          <Image
+                            className="w-full h-full bg-blue-300"
+                            src={""}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div></div>
+                </div>
+              </div>
             </li>
 
             <li className="relative h-16 pt-[1.2rem] px-2 group duration-300 hover:bg-blue-50 hover:border-b-2 hover:border-blue-500">
